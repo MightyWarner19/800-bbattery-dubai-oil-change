8c49598274a8b2df0814f52fa097a326179a9ac2
Shahid Bashir <shahid.bashir@mightywarner.ae>
2025-01-08 09:47:27 +0400

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-// next.config.js
 module.exports = {
+  // Enabling React Strict Mode
+  reactStrictMode: true,
+
+  // Configure Next.js to use a custom base path
+  basePath: '/best-onsite-mobile-car-services-dubai',
+
+  // Path for static assets
+  assetPrefix: '/best-onsite-mobile-car-services-dubai/',
+
+  // For serving images correctly
   images: {
-    domains: ['imagedelivery.net'], 
+    loader: 'custom', // Using a custom loader since you're using a CDN
+    domains: ['imagedelivery.net'], // Add your CDN domain here
+    unoptimized: true, // Disable Next.js's built-in image optimization since CDN handles it
+  },
+
+  // Webpack configuration to ensure images and static assets are correctly processed
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false, // Ensure `fs` module is disabled on the client side
+      };
+    }
+    return config;
   },
 };
